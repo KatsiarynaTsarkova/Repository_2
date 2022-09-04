e5c4ceb6942865a0e5469c2d50e485412d7a3780
Kate <katya_tsarkov@mail.ru>
2022-09-04 17:40:24 +0300

    Example003

---
+++ Examples/Example003/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("ВВедите число от 1 до 10 ");
+string name = Console.Readline();
+int number = Convert.ToInt32(name);
+if(number%2 = 0)
+{
+    Console.Writeline("Четное число");
+}
+else if(number%2 > 0)
+{
+    Console.Writeline("Нечетное число");
+}
