e004d363c79f35d371c2ed420bc7cb22a31c2f50
Kate <katya_tsarkov@mail.ru>
2022-09-04 13:21:31 +0300

    Example001

---
+++ Examples/Example001/Program.cs
@@ -0,0 +1,13 @@
+﻿int numberA = 5;
+int numberB = 7;
+
+int max = numberA;
+
+if(numberB > max)
+{
+ Console.WriteLine("Число B больше A");   
+}
+else
+{
+  Console.WriteLine("Число A больше B");  
+}
